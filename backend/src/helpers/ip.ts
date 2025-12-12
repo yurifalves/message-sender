ead457ae44e7f44468e1f6dc92565fe8a5880923
yuri <yurifuchidaalves@gmail.com>
2025-12-12 17:58:18 -0300

    arrumando o inicio do projeto

--- backend/src/helpers/ip.ts
+++ backend/src/helpers/ip.ts
@@ -29,20 +29,22 @@ interface LocationResponse {
 }
 
 async function getServerIPv4(): Promise<string> {
-  const response: Response = await fetch("ttps://api4.ipify.org");
-  if (!response.ok) throw new Error();
-  return response.text();
+  const response: Response = await fetch("https://api4.ipify.org");
+  if (!response.ok) throw new Error("Erro ao obter IPv4.");
+  const ipv4: string = await response.text();
+  return ipv4;
 }
 
 async function getServerIPv6(): Promise<string> {
   const response: Response = await fetch('https://api6.ipify.org');
-  if (!response.ok) throw new Error();
-  return response.text();
+  if (!response.ok) throw new Error("Erro ao obter IPv6.");
+  const ipv6: string = await response.text();
+  return ipv6;
 }
 
 async function getLocation(ipAddress: string): Promise<LocationResponse> {
   const apiKey: string = '72050ebbf7134d2a8411394a1eb321b0';
   const response: Response = await fetch(`https://vpnapi.io/api/${ipAddress}?key=${apiKey}`);
-  if (!response.ok) throw new Error('Erro ao obter Localização.');
+  if (!response.ok) throw new Error("Erro ao obter Localização.");
   return response.json();
 }
